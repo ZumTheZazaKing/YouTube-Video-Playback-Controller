5ee67c3e370dc848f3b23efcc164d9a36b9c1c4e
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2021-02-06 23:55:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const speed = document.querySelector('#speed');
+
+const bar = document.querySelector('#speed-bar');
+
+const video = document.querySelector('#video');
+
+speed.addEventListener('mousemove', function(e){
+
+    const y = e.pageY - this.offsetTop;
+
+    const percent = y / this.offsetHeight;
+
+    const min = 0.6;
+
+    const max = 4.01;
+
+    const height = Math.round(percent * 100) + '%';
+
+    const playbackRate = percent * (max - min) + min;
+
+
+    bar.style.height = height;
+
+    bar.textContent = playbackRate.toFixed(2) + 'x';
+
+    if(bar.style.height < 19){
+
+        bar.style.height = 19 + '%';
+
+    }
+
+    video.playbackRate = playbackRate;
+
+    console.log(height);
+
+})
+
+
+//
+//
+//The following code is to for finding the youtube video which belongs to user URL
+//
+//
+
+function searchVideo(){
+
+    let userURL = document.getElementById('youtubeURL').value;
+
+    //https://www.youtube.com/watch?v=bIkoo4YgTAs
+
+    let firstPart = userURL.slice(0, 24);
+
+    let secondPart = userURL.slice(24,);
+
+    let userTrueURL = firstPart + 'embed/' + secondPart;
+
+    video.src = userTrueURL;
+
+}
+
+const searchButton = document.getElementById('search');
+
+searchButton.addEventListener('click', searchVideo);
